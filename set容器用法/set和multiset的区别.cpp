69f823409dc5e574d8822acf74b9bd1aae8e72e3
yuye05 <3474682031@qq.com>
2026-06-07 11:55:55 +0800

    set和multiset

---
+++ set容器用法/set和multiset的区别.cpp
@@ -0,0 +1,34 @@
+#include "set.h"
+
+void test7()
+{
+	set<int> s1;
+	//s1.insert(10);
+	pair<set<int>::iterator, bool> ret = s1.insert(10);
+
+	if (ret.second)
+		cout << "第一次插入成功！" << endl;
+	else
+		cout << "第一次插入失败" << endl;
+
+	ret = s1.insert(10);
+	if (ret.second)
+		cout << "第二次插入成功！" << endl;
+	else
+		cout << "第二次插入失败" << endl;
+
+	Print(s1);
+
+	multiset<int> s2;
+	///允许插入多个
+	s2.insert(20);
+	s2.insert(20);
+
+	for (multiset<int>::iterator it = s2.begin(); it != s2.end(); it++)
+	{
+		cout << *it << " ";
+	}
+	cout << endl;
+
+
+}
